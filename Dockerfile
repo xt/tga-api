2e0e3fe28e2ff5e17043249bcc5bb0053bc11f0e
Vinci Rufus <vinci.rufus@gmail.com>
2020-07-08 11:57:30 +0100

    commit1

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
-FROM node:12
-WORKDIR /usr/src/app
+# stage 1
+FROM node:12-alpine AS base
+
+WORKDIR /app
 COPY . .
 RUN yarn
 RUN yarn build
 
+# stage 2
+
+FROM node:12-alpine
+COPY --from=base /app/package.json .
+COPY --from=base /app/dist ./dist
+RUN npm install express
 EXPOSE 8000
 CMD yarn start
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import express, { Request, Response } from "express";
-import planes from "./data/planes.json";
+import express, { Request, Response } from 'express';
+import planes from './data/planes.json';
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("Hello TGA \n");
+server.get('/', (req, res) => {
+    res.send('Hello TGA Try 2 \n');
 });
 
-server.get("/api/get-planes", (req, res) => {
-  res.status(200).json(planes);
+server.get('/api/get-planes', (req, res) => {
+    res.status(200).json(planes);
+});
+
+server.get('/api/get-planes-details', (req, res) => {
+    res.status(200).json(planes);
 });
 
 export { server };
